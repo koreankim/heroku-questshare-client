10e52ecee3971bb8c6302c2cfee6c3787f2adab7
Junho Kim <jnhkm311@gmail.com>
2020-04-28 22:05:16 -0700

    Add donate button and fix gitignore

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout } from 'antd';
+import { Layout, Tooltip } from 'antd';
 import "./App.css";
 import Navbar from "../navbar/Navbar";
 import { appRouter } from "../../utils/router/AppRouter";
@@ -8,6 +8,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 const { Content, Footer } = Layout;
 
 class App extends React.Component {
+
+  displayDonateButton = () => {
+    return (
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_donations" />
+        <input type="hidden" name="business" value="PVR5B7F8A56DJ" />
+        <input type="hidden" name="currency_code" value="CAD" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+    )
+  }
+
   render() {
     return (
       <Router forceRefresh={true}>
@@ -19,6 +32,7 @@ class App extends React.Component {
             </div>
           </Content>
           <Footer style={{ textAlign: "center" }}>
+            {this.displayDonateButton()}
             QuestShare ©2020 Created by Junho Kim
           </Footer>
         </Layout>
